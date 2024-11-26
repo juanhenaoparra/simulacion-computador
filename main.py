1fe687a29f2d3b46a34a9cf15ebf7b935e0fb99c
mauricio <tu@email.com>
2024-11-26 09:57:11 -0500

    Carga de instrucciones a la memoria de programa

--- main.py
+++ main.py
@@ -1,26 +1,15 @@
-from cpu.control_unit.control_unit import ControlUnit, ControlUnitMode
-from cpu.memory.memory import Memory, MemoryType
-from cpu.models.directions import number_to_binary
-from cpu.models.events import EventBus
-
-
+import tkinter as tk
+from interfaz.carga_instrucciones import TextStorageApp
 def main():
-    mp = Memory(MemoryType.PROGRAM)
-    mp.write(
-        number_to_binary(0, 28),  # instruction at 0
-        "0100" + "00" + number_to_binary(1, 28) + "10" + number_to_binary(2, 28),
-    )
-    mp.write(
-        number_to_binary(1, 28),
-        "0000" + "00" + number_to_binary(3, 28) + "01" + number_to_binary(4, 28),
-    )
-
-    EventBus.set_debug(True)
-
-    control_unit = ControlUnit()
-    print("Running...")
-    control_unit.run(mode=ControlUnitMode.RUN, delay=3)
-
-
+    """
+    Función principal que inicializa la ventana principal de la aplicación
+    y carga la interfaz de usuario.
+    """
+    # Crear la ventana principal
+    root = tk.Tk()
+    app = TextStorageApp(root)
+    
+    # Iniciar el bucle principal de la interfaz gráfica
+    root.mainloop()
 if __name__ == "__main__":
     main()
